ad9103b4c1c29ed6b6805711f3741290f12b81aa
student <student@kas-it21-pc41>
2022-10-05 16:25:09 +0900

    sample作成

---
+++ src/main/java/sample.java
@@ -0,0 +1,14 @@
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class sample
+ */
+@WebServlet("/sample")
+public class sample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+}
