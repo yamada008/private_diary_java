fca1823b28fd423dc0706b47e2c5146baf5513c6
student <student@kas-it21-pc41>
2022-10-06 14:16:46 +0900

    色々編集した

---
+++ src/main/java/blog/indexservlet.java
@@ -0,0 +1,13 @@
+package blog;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class indexservlet
+ */
+@WebServlet("/indexservlet")
+public class indexservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
--- src/main/java/sample.java
+++ src/main/java/sample.java
@@ -1,7 +1,12 @@
 
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class sample
@@ -10,5 +15,12 @@ import javax.servlet.http.HttpServlet;
 public class sample extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
+	 protected void doGet(HttpServletRequest req,
+		      HttpServletResponse resp)
+		      throws ServletException, IOException {
+	
+	req.getRequestDispatcher("/WEB-INF/jsp/.jsp").forward(req, resp);
+	
+	 }
 	
 }
