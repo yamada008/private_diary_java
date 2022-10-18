4803ecb39dacc6ed18fcf30e7622d895c35bd667
gotou-katsuki <github0606>
2022-10-18 09:56:25 +0900

    作った

---
+++ src/main/java/servlet/Sample.java
@@ -0,0 +1,13 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class Sample
+ */
+@WebServlet("/Sample")
+public class Sample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
