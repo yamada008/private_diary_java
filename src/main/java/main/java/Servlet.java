1ffe3c6b3924b275aedc4a06cdf6b9bdab865c88
sinnya188 <kai1212ri@gmail.com>
2022-10-18 10:35:00 +0900

    sevletをやった

---
+++ src/main/java/main/java/Servlet.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
